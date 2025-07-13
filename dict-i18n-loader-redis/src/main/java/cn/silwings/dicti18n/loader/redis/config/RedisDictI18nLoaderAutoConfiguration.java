04f78ed17c18069e73edf3a47069d3c101aaa30f
Silwings <1910506332@qq.com>
2025-07-14 00:10:37 +0800

    Redis Loader init

---
+++ dict-i18n-loader-redis/src/main/java/cn/silwings/dicti18n/loader/redis/config/RedisDictI18nLoaderAutoConfiguration.java
@@ -0,0 +1,41 @@
+package cn.silwings.dicti18n.loader.redis.config;
+
+import cn.silwings.dicti18n.loader.redis.impl.ReactiveRedisDictI18nLoader;
+import cn.silwings.dicti18n.loader.redis.impl.RedisDictI18nLoader;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+@ConditionalOnMissingBean(RedisDictI18nLoader.class)
+public class RedisDictI18nLoaderAutoConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "dict-i18n.loader.redis")
+    public DictI18nRedisProperties dictI18nRedisProperties() {
+        return new DictI18nRedisProperties();
+    }
+
+    // 响应式版本优先注入
+    @Bean
+    @ConditionalOnBean(ReactiveStringRedisTemplate.class)
+    public ReactiveRedisDictI18nLoader reactiveRedisDictI18nLoader(
+            DictI18nRedisProperties dictI18nRedisProperties,
+            ReactiveStringRedisTemplate reactiveRedisTemplate) {
+        return new ReactiveRedisDictI18nLoader(dictI18nRedisProperties, reactiveRedisTemplate);
+    }
+
+    // 否则注入阻塞式版本
+    @Bean
+    @ConditionalOnMissingBean(ReactiveRedisDictI18nLoader.class)
+    public RedisDictI18nLoader redisDictI18nLoader(
+            DictI18nRedisProperties dictI18nRedisProperties,
+            StringRedisTemplate stringRedisTemplate) {
+        return new RedisDictI18nLoader(dictI18nRedisProperties, stringRedisTemplate);
+    }
+
+}
---
+++ dict-i18n-loader-redis/src/main/java/cn/silwings/dicti18n/loader/redis/impl/RedisDictI18nLoader.java
@@ -0,0 +1,31 @@
+package cn.silwings.dicti18n.loader.redis.impl;
+
+
+import cn.silwings.dicti18n.loader.DictI18nLoader;
+import cn.silwings.dicti18n.loader.redis.config.DictI18nRedisProperties;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Optional;
+
+public class RedisDictI18nLoader implements DictI18nLoader {
+
+    private final DictI18nRedisProperties dictI18nRedisProperties;
+
+    private final StringRedisTemplate redisTemplate;
+
+    public RedisDictI18nLoader(final DictI18nRedisProperties dictI18nRedisProperties, final StringRedisTemplate redisTemplate) {
+        this.dictI18nRedisProperties = dictI18nRedisProperties;
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public String loaderName() {
+        return "redis";
+    }
+
+    @Override
+    public Optional<String> get(final String lang, final String key) {
+        final String redisKey = String.format("dict_i18n:%s:%s", lang, key);
+        return Optional.ofNullable(this.redisTemplate.opsForValue().get(redisKey));
+    }
+}
